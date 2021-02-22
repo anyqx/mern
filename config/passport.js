cd993c83a66302bfad17c08ab9168f2b06cfcd40
anyqx <annieyxu@berkeley.edu>
2021-02-22 09:00:02 -0800

    tweets

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,16 @@
-const JustStrategy
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('./keys');
+
+const options = {};
+options.JwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+        console.log(jwt_payload);
+        done();
+    }))
+}
