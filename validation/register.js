452f9ce1458b18e77d61f9715de1b7b2bc0ada0a
anyqx <annieyxu@berkeley.edu>
2021-02-22 07:20:37 -0800

    valication users

--- validation/register.js
+++ validation/register.js
@@ -36,7 +36,7 @@ module.exports = function validateRegisterInput(data) {
     }
 
     if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = 'Passwords must matc';
+        errors.password2 = 'Passwords must match';
     }
 
     return {
