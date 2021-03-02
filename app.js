749399be9b095805e3d6f7cb9f06b0b9ddae6868
anyqx <annieyxu@berkeley.edu>
2021-03-02 11:39:42 -0800

    push

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
 const express = require("express");
 const app = express();
-
-
-
 const mongoose = require("mongoose"); //in the video
-const db=require("./config/keys").mongoURI;
-
+const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const User = require('./models/User');
 const bodyParser = require('body-parser');
+const User = require('./models/User');
+const passport = require('passport');
+
+const path = require('path');
 
 mongoose //in the video
-.connect(db, { useNewUrlParser: true}, )
-.then(() => console.log("Connected to mongoDB"))
-.catch( err => console.log(err))
+    .connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(err));
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.get("/",(req, res)=> {
-    const user = new User({
-        handle: 'Annie',
-        email: 'annie@annie.com',
-        password: 'AnnieisAmazing'
-    })
+    // const user = new User({
+    //     handle: 'Annie',
+    //     email: 'annie@annie.com',
+    //     password: 'AnnieisAmazing'
+    // })
     user.save()
     console.log(res);
     res.send("Hello World!")
 })
 
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000; 
 
